15610b23322361ada6575588fb3cd9df3ea32f20
John <johnitolee@gmail.com>
2017-09-16 15:14:09 -0700

    add calculator

---
+++ calculator_demo/calculator_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator_demo
+//
+//  Created by John  Ito lee on 9/16/17.
+//  Copyright © 2017 Johnito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
