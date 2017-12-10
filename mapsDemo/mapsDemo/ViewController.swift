986c2057e4f35882c6f220b09c7f675d33d6531a
John <johnitolee@gmail.com>
2017-12-09 17:12:01 -0800

    add annotation

--- mapsDemo/mapsDemo/ViewController.swift
+++ mapsDemo/mapsDemo/ViewController.swift
@@ -16,19 +16,46 @@ class ViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let latitude: CLLocationDegrees = 14.7
-        let longitude: CLLocationDegrees = -73.9
+        
+        // setting the map view to a location
+        let latitude: CLLocationDegrees = 37.7749
+        let longitude: CLLocationDegrees = -122.4194
         
         let latDelta: CLLocationDegrees = 0.05
         let longDelta: CLLocationDegrees = 0.05
         
         let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
         
-        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         
-        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: coordinate, span: span)
         
         map.setRegion(region, animated: true)
+        
+        // setting down an annotation
+        let annotation = MKPointAnnotation()
+        annotation.title = "San Francisco"
+        annotation.subtitle = "I Will go here one day."
+        annotation.coordinate = coordinate
+        map.addAnnotation(annotation)
+        
+        // setting a long press gesture
+        let uiLongPress = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longpress(gestureRecognizer:)))
+        uiLongPress.minimumPressDuration = 2
+        map.addGestureRecognizer(uiLongPress)
+    }
+    
+    func longpress(gestureRecognizer: UIGestureRecognizer){
+        let touchPoint = gestureRecognizer.location(in: self.map)
+        print(touchPoint)
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+        print(coordinate)
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "New Place"
+        annotation.subtitle = "I Will go here one day."
+        annotation.coordinate = coordinate
+        map.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
