2546fb493ba5769cdf577a1025422d75ad75cfce
John <johnitolee@gmail.com>
2017-12-09 16:26:12 -0800

    add map

---
+++ mapsDemo/mapsDemo/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  mapsDemo
+//
+//  Created by John  Ito lee on 12/9/17.
+//  Copyright © 2017 Johnito. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let latitude: CLLocationDegrees = 14.7
+        let longitude: CLLocationDegrees = -73.9
+        
+        let latDelta: CLLocationDegrees = 0.05
+        let longDelta: CLLocationDegrees = 0.05
+        
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        map.setRegion(region, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
